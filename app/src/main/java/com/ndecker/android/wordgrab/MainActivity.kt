1743371b98a547ae5061fcbe5a5bcf02c98dc2e7
Brad <broswen@gmail.com>
2020-04-30 12:15:15 -0500

    test

--- app/src/main/java/com/ndecker/android/wordgrab/MainActivity.kt
+++ app/src/main/java/com/ndecker/android/wordgrab/MainActivity.kt
@@ -9,7 +9,6 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.main_activity)
-        //this is a gitignore test
         if (savedInstanceState == null) {
             supportFragmentManager.beginTransaction()
                     .replace(R.id.container, MainFragment.newInstance())
